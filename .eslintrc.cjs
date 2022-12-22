af3b222c45859dc0ad057fd5e18c301409519f28
ZhaoCe <17631807110@163.com>
2022-12-22 18:14:13 +0800

    eslint 配置

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
