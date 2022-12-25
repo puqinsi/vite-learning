32e30d34ce9b01b9c32de357b1a1e7ba90a3b7a8
ZhaoCe <17631807110@163.com>
2022-12-25 20:07:45 +0800

    feat: 别名&图片压缩

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import { Header } from './header/index';
-
+import { ReactComponent as ReactLogo } from '@assets/react.svg';
+import { version } from '../package.json';
+console.log(version);
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div className='App'>
-            <Header />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Header />
+      <ReactLogo />
+    </div>
+  );
 }
 
 export default App;
